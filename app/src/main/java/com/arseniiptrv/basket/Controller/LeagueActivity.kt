671334a9d4cbb98e25122a84481a6f66ef37c87a
Arsenii Petrov <arseniiptrv@gmail.com>
2020-03-28 22:24:55 +0200

    set up instance state. Added ic launcer

--- app/src/main/java/com/arseniiptrv/basket/Controller/LeagueActivity.kt
+++ app/src/main/java/com/arseniiptrv/basket/Controller/LeagueActivity.kt
@@ -13,10 +13,23 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle) { // safe call???
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
+        }
+    }
+
 
     fun onMensClicked(view: View) {
         womensLeagueBtn.isChecked = false
