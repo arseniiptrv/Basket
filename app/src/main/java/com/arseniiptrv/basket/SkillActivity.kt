c6e9a4733c3c39c70324396bd7a21de2b67265b4
Arsenii Petrov <arseniiptrv@gmail.com>
2020-03-28 14:50:41 +0200

    made up third activity (activity_skill). Created intent to move to 3d activity from 2d

---
+++ app/src/main/java/com/arseniiptrv/basket/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.arseniiptrv.basket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
